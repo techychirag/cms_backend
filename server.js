03b74a0594397f1d7f89191448d5ee2c5808074d
techychirag <84778632+techychirag@users.noreply.github.com>
2021-05-27 10:02:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const route = require("./route")
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
 const path = require('path')
-
+const cors = require('cors')
 
 mongoose.connect("mongodb+srv://Chirag:mystar3333@cluster0.thc18.mongodb.net/CMS_Database?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>
     {
+        
+       
         const app=express();
-
+        app.use(cors())
         app.use(bodyParser.json());
         app.use(express.json())
         app.use('/uploads',express.static(__dirname + '/uploads'))
